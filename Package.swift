365743902efc1c93730757cea288bef4b90637a0
Erik Little <nuclear.ace@gmail.com>
2017-09-20 07:54:55 -0400

    swift 4

--- Package.swift
+++ Package.swift
@@ -1,6 +1,14 @@
+// swift-tools-version:4.0
+
 import PackageDescription
 
 let package = Package(
     name: "COPUS",
-    pkgConfig: "opus"
+    pkgConfig: "opus",
+    products: [
+        .library(name: "COPUS", targets: ["COPUS"])
+    ],
+    targets: [
+        .target(name: "COPUS", publicHeadersPath: "./shim.h")
+    ]
 )
