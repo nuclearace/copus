bf5c89f98faad85e0aa87152f9e6b01df9b9cbfe
Tellow Krinkle <tellowkrinkle@gmail.com>
2019-03-01 20:17:18 -0600

    Fix encoder macros on decoders Didn't realize that some of the macros are only for encoders

--- shim.h
+++ shim.h
@@ -3,17 +3,31 @@
 
 #include <opus.h>
 
-#define OPUS_SHIM_CTL(macroname, funcname, vartype, varname) \
+#define OPUS_SHIM_GENERIC_CTL0(macroname, funcname) \
+inline int opus_encoder_##funcname(OpusEncoder *st) {\
+	return opus_encoder_ctl(st, OPUS_##macroname);\
+}
+
+#define OPUS_SHIM_GENERIC_CTL1(macroname, funcname, vartype, varname) \
 inline int opus_encoder_##funcname(OpusEncoder *st, vartype varname) {\
 	return opus_encoder_ctl(st, OPUS_##macroname(varname));\
 }
 
+#define OPUS_SHIM_ENCODER_CTL1(macroname, funcname, vartype, varname) OPUS_SHIM_GENERIC_CTL1(macroname, funcname, vartype, varname)
+
 #include "ctl_shim.h"
 
-#define OPUS_SHIM_CTL(macroname, funcname, vartype, varname) \
+#define OPUS_SHIM_GENERIC_CTL0(macroname, funcname) \
+inline int opus_decoder_##funcname(OpusDecoder *st) {\
+	return opus_decoder_ctl(st, OPUS_##macroname);\
+}
+
+#define OPUS_SHIM_GENERIC_CTL1(macroname, funcname, vartype, varname) \
 inline int opus_decoder_##funcname(OpusDecoder *st, vartype varname) {\
 	return opus_decoder_ctl(st, OPUS_##macroname(varname));\
 }
+
+#define OPUS_SHIM_ENCODER_CTL1(macroname, funcname, vartype, varname)
 
 #include "ctl_shim.h"
 
