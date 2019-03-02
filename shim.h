9633e61c975d25db2bd98fdf613bbbcf531bb0dc
Tellow Krinkle <tellowkrinkle@gmail.com>
2019-03-01 19:55:55 -0600

    Add lots of functions that forward to opus_[en|de]coder_ctl

--- shim.h
+++ shim.h
@@ -3,4 +3,18 @@
 
 #include <opus.h>
 
+#define OPUS_SHIM_CTL(macroname, funcname, vartype, varname) \
+inline int opus_encoder_##funcname(OpusEncoder *st, vartype varname) {\
+	return opus_encoder_ctl(st, OPUS_##macroname(varname));\
+}
+
+#include "ctl_shim.h"
+
+#define OPUS_SHIM_CTL(macroname, funcname, vartype, varname) \
+inline int opus_decoder_##funcname(OpusDecoder *st, vartype varname) {\
+	return opus_decoder_ctl(st, OPUS_##macroname(varname));\
+}
+
+#include "ctl_shim.h"
+
 #endif
